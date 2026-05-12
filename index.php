0322210aac93283b59a821434961ca2dc172256f
Gab-off <albuquerque1.gabriel@gmail.com>
2026-05-11 21:58:11 -0300

    First version of form job complete, with button to add and remove service

--- index.php
+++ index.php
@@ -19,7 +19,54 @@ if (!isset($_SESSION['logado'])) {
     <title>Página principal</title>
 </head>
 <body>
-<h1>Parabéns <?= $_SESSION['username'] ?>, você está logado</h1>
+<h1>Bem vindo <?= $_SESSION['username'] ?></h1>
+<h2>Crie o seu orçamento: </h2>
+<form action="">
+    <div class="listaServicos">
+        <div class="itemServico">
+            <label>
+                <input type="text" name="descricao[]" placeholder="Descreva o serviço" required>
+            </label>
+
+            <select name="tipoCalculo" id="">
+                <option value="global">Preço Global</option>
+                <option value="fixo">Preço Fixo</option>
+                <option value="metro">Por Metro (m²)</option>
+            </select>
+            <br/>
+            <label>
+                <input type="number" name="quantidade[]" placeholder="Qtd" step="0.01">
+            </label>
+            <label >
+                <input type="number" name="valor[]" placeholder="R$" step="0.01">
+            </label>
+            <br/>
+            <br/>
+        </div>
+    </div>
+
+    <button type="button" id="btn-adicionar">+ Adicionar novo serviço</button>
+    <button type="button" id="btn-remover">- remover último serviço</button>
+    <br>
+    <br>
+    <input type="submit" value="Gerar Orçamento">
+</form>
 <a href="functions/logout.php">Sair</a>
 </body>
+<script>
+    document.getElementById('btn-adicionar').addEventListener('click', function () {
+        let listaServicos = document.querySelector('.listaServicos');
+        let itemServico = listaServicos.querySelector('.itemServico:last-child').cloneNode(true);
+        itemServico.querySelectorAll('input').forEach(input => input.value = '');
+        listaServicos.appendChild(itemServico);
+    });
+
+    document.getElementById('btn-remover').addEventListener('click', function () {
+        let listaServicos = document.querySelector('.listaServicos');
+        let itemServico = listaServicos.querySelector('.itemServico:last-child');
+        if (itemServico) {
+            listaServicos.removeChild(itemServico);
+        }
+    })
+</script>
 </html>
